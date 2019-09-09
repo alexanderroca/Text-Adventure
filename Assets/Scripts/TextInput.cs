3ceed3430b1bc9020c29e1864fc617019d6be836
alexanderroca <alex.rocanv@gmail.com>
2019-09-09 16:42:45 +0200

    First commit

---
+++ Assets/Scripts/TextInput.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextInput : MonoBehaviour
+{
+    public InputField inputField;
+
+    private GameController controller;
+
+    void Awake()
+    {
+        controller = GetComponent<GameController>();
+        inputField.onEndEdit.AddListener(AcceptStringInput);
+    }
+
+    void AcceptStringInput(string userInput)
+    {
+        userInput = userInput.ToLower();
+        controller.LogStringWithReturn(userInput);
+
+        char[] delimiterCharacters = { ' ' };
+        string[] separatedInputWords = userInput.Split(delimiterCharacters);
+
+        for (int i = 0; i < controller.inputActions.Length; i++)
+        {
+            InputAction inputAction = controller.inputActions[i];
+            if (inputAction.keyWord == separatedInputWords[0])
+            {
+                inputAction.RespondToInput(controller, separatedInputWords);
+            }   //if
+        }   //for
+
+        InputComplete();
+    }
+
+    void InputComplete()
+    {
+        controller.DisplayLogText();
+        inputField.ActivateInputField();
+        inputField.text = null;
+    }
+}
